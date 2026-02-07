300ff783f229726ca39e8edfda089b995eeab0cd
Blind Guess Senior <2820821574@qq.com>
2026-02-08 01:24:52 +0800

    chore(Runtime/Core/Event/EventBuses/DefaultEventBus.cs): more comment

--- Runtime/Core/Event/EventBuses/DefaultEventBus.cs
+++ Runtime/Core/Event/EventBuses/DefaultEventBus.cs
@@ -2,6 +2,9 @@ using UnityEngine.Scripting;
 
 namespace Core.Event.EventBuses
 {
+    /// <summary>
+    /// This is a sample of EventBus. You can just make your own event bus with the same pattern as this one.
+    /// </summary>
     [Preserve]
     public class DefaultEventBus : EventBus<DefaultEventBus>
     {
