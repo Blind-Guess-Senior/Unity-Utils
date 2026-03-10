ee2ae76e6ebc0f7b82fc11e6eb5855b3d29f5990
Blind-Guess-Senior <94767867+Blind-Guess-Senior@users.noreply.github.com>
2026-03-10 23:23:08 +0800

    feat(R/Utilities/DataStructure/QuadTree/QuadTreeUtils): add utils for rect and gameobject

--- Runtime/Utilities/DataStructure/QuadTree/QuadTreeUtils.cs
+++ Runtime/Utilities/DataStructure/QuadTree/QuadTreeUtils.cs
@@ -1,12 +1,56 @@
+#if __ARTIFACT_UNITY_UTILS__QUADTREE_ENABLED
 using UnityEngine;
 
-namespace Utilities.DataStructure.QuadTree
+// ReSharper disable CheckNamespace
+
+namespace Artifact.UnityUtils.Utilities.DataStructure.QuadTree
 {
     /// <summary>
     /// Utils class for QuadTree. Especially for TItem = Collider2D situation.
     /// </summary>
     public static class QuadTreeUtils
     {
+        #region Rect Utils
+
+        /// <summary>
+        /// Get rect by Unity world coordinate.
+        /// </summary>
+        /// <param name="minX">The left x pos.</param>
+        /// <param name="maxX">The right x pos.</param>
+        /// <param name="minY">The bottom y pos.</param>
+        /// <param name="maxY">The top y pos.</param>
+        /// <returns>The rect of given range.</returns>
+        public static Rect GetRect(float minX, float minY, float maxX, float maxY)
+            => new Rect(minX, -maxY, maxX - minX, maxY - minY);
+
+        public static Rect GetRect(int minX, int minY, int maxX, int maxY)
+            => GetRect((float)minX, (float)minY, (float)maxX, (float)maxY);
+
+        public static Rect GetRect(double minX, double minY, double maxX, double maxY)
+            => GetRect((float)minX, (float)minY, (float)maxX, (float)maxY);
+
+        /// <summary>
+        /// Get rect by Unity world coordinate.
+        /// </summary>
+        /// <param name="leftBottom">The Vector2 position in left-bottom.</param>
+        /// <param name="rightTop">The Vector2 position in right-top.</param>
+        /// <returns>The rect of given range.</returns>
+        public static Rect GetRect(Vector2 leftBottom, Vector2 rightTop)
+            => GetRect(leftBottom.x, leftBottom.y, rightTop.x, rightTop.y);
+
+        public static Rect GetRect(Vector2 leftBottom, float maxX, float maxY)
+            => GetRect(leftBottom.x, leftBottom.y, maxX, maxY);
+
+        public static Rect GetRect(Vector2 leftBottom, int maxX, int maxY)
+            => GetRect(leftBottom.x, leftBottom.y, maxX, maxY);
+
+        public static Rect GetRect(Vector2 leftBottom, double maxX, double maxY)
+            => GetRect(leftBottom.x, leftBottom.y, maxX, maxY);
+
+        #endregion
+
+        #region Collider2D Utils
+
         /// <summary>
         /// Helper method for getting Collider2D item's rect.
         /// </summary>
@@ -35,5 +79,43 @@ namespace Utilities.DataStructure.QuadTree
         /// <returns>True if they collide; otherwise, false.</returns>
         public static bool CollisionResultOf(Collider2D coa, Collider2D cob)
             => coa.IsTouching(cob);
+
+        #endregion
+
+        #region GameObject Utils
+
+        /// <summary>
+        /// Helper method for getting GameObject item's Collider2D component's rect.
+        /// </summary>
+        /// <param name="item">The GameObject item to getting rect.</param>
+        /// <returns>The rect of given GameObject item.</returns>
+        public static Rect GetGameObjectRect(GameObject item)
+        {
+            var co = item.GetComponent<Collider2D>();
+
+            var itemBounds = co.bounds;
+            // IMPORTANT: the coordinate system of rect and Unity world is different:
+            // Rect                         Unity
+            // minx,miny    maxx,miny      minx,maxy    maxx,maxy
+            // 
+            // minx,maxy    maxx,maxy      minx,miny    maxx,miny
+            // And Rect's y-axis is mirror to 0 compare with Unity's.
+            var itemRect = new Rect(itemBounds.min.x, -itemBounds.max.y,
+                itemBounds.size.x, itemBounds.size.y);
+
+            return itemRect;
+        }
+
+        /// <summary>
+        /// Helper method for getting collision result of two GameObject items by checking their Collider2D components.
+        /// </summary>
+        /// <param name="coa">The GameObject item to check.</param>
+        /// <param name="cob">The GameObject item to check.</param>
+        /// <returns>True if they collide; otherwise, false.</returns>
+        public static bool CollisionResultOf(GameObject coa, GameObject cob)
+            => coa.GetComponent<Collider2D>().IsTouching(cob.GetComponent<Collider2D>());
+
+        #endregion
     }
 }
+#endif
